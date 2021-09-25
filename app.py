605631bca26ae359c589bb446a69fc201a521794
Sakshat682 <56864491+Sakshat682@users.noreply.github.com>
2021-09-26 00:21:52 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
 from flask import Flask, render_template, request
-import jsonify
-import requests
+# import jsonify
+# import requests
 import pickle
-import numpy as np
+# import numpy as np
 import sklearn
-from sklearn.preprocessing import StandardScaler
 app = Flask(__name__)
 model = pickle.load(open('decision_trees_model.pkl', 'rb'))
 @app.route('/',methods=['GET'])
